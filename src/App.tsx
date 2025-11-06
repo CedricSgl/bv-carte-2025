7633db83717fbf33942dc40c7269c4be30e2d928
Cédric <cedric.singele@gmail.com>
2025-11-06 22:44:25 +0100

    feat: initialize project with Vite, React, and TypeScript setup
    - Added package.json with dependencies and scripts for development, build, linting, and preview.
    - Created Vite configuration file for project setup.
    - Implemented TypeScript configuration files for app and node environments.
    - Added main entry point (main.tsx) and index.css for global styles.
    - Developed core components including H1, H2, Accordion, and Button with respective styles.
    - Created App component to display a menu with categories and items using Accordion.
    - Included SVG assets for React and Vite logos.
    - Added utility functions for class name management.
    - Established Tailwind CSS for styling and animations.
    - Set up TypeScript strict mode for better type safety.

---
+++ src/App.tsx
@@ -0,0 +1,241 @@
+import './App.css'
+import { H1 } from './components/h1'
+import { H2 } from './components/h2'
+import {
+  Accordion,
+  AccordionContent,
+  AccordionItem,
+  AccordionTrigger
+} from './components/ui/accordion'
+
+type MenuItem = {
+  name: string
+  abv?: string
+  price?: string
+  description?: string
+  imageUrl?: string
+}
+
+type MenuCategory = {
+  name: string
+  size?: string
+  items: MenuItem[]
+}
+
+const menuItems: MenuCategory[] = [
+  {
+    name: 'Bières au fût',
+    size: '25cl',
+    items: [
+      {
+        name: 'La trappe Puur',
+        abv: '4.5%',
+        price: '2,5€',
+        description: 'Bière blonde légèrement trouble, fruitée et houblonnée.'
+      },
+      {
+        name: 'La Bierbeek "Saison"',
+        abv: '5.5%',
+        price: '2,5€',
+        description: "Bière à l'amertume douce, légèrement fruitée et épicée."
+      }
+    ]
+  },
+  {
+    name: 'Bières en bouteille',
+    size: '33cl',
+    items: [
+      {
+        name: 'Placebo (Lupulus)',
+        abv: '0.0%',
+        price: '3,5€',
+        description:
+          "Bière à l'amertume subtile et aux notes gustatives complexes."
+      },
+      {
+        name: 'Wunder Lager',
+        abv: '3.8%',
+        price: '3,5€',
+        description: 'Blonde légère en alcool, riche en houblons aromatiques.'
+      },
+      {
+        name: 'Fructus (Lupulus)',
+        abv: '4.2%',
+        price: '3,5€',
+        description: 'Bière rouge fruitée, légèrement sucrée.'
+      },
+      {
+        name: 'La Tharée Légère',
+        abv: '4.5%',
+        price: '4€',
+        description: 'Blonde légère et rafraîchissante.'
+      },
+      {
+        name: 'Corsendonk blonde',
+        abv: '6.5%',
+        price: '4€',
+        description: 'Bière "Belgian Pale Ale".'
+      },
+      {
+        name: 'Hopera (Lupulus)',
+        abv: '6%',
+        price: '4€',
+        description: 'Bière blonde type IPA.'
+      },
+      {
+        name: 'Jambe de bois',
+        abv: '8%',
+        price: '4€',
+        description: "Blonde triple à l'amertume prononcée."
+      },
+      {
+        name: 'Natte Lore',
+        abv: '9.1%',
+        price: '4€',
+        description: "Blonde forte aux aromes et à l'amertume prononcée."
+      },
+      {
+        name: 'Witkap Pater Brune',
+        abv: '8%',
+        price: '4€',
+        description: 'Brune "double", douce.'
+      }
+    ]
+  },
+  {
+    name: 'Grandes bouteilles',
+    size: '75cl',
+    items: [
+      {
+        name: 'La Lustrée (Tharée)',
+        abv: '6.4%',
+        price: '9€',
+        description:
+          'Pale Ale aux saveurs puissantes, avec une amertume bien présente et des notes distinctes de pin Douglas et de pamplemousse.'
+      },
+      {
+        name: 'La Tharée Triple',
+        abv: '8%',
+        price: '9€',
+        description: 'Bière forte aux notes agrumeuses de douglas.'
+      },
+      {
+        name: 'Lupulus Blonde',
+        abv: '8.5%',
+        price: '9€',
+        description:
+          'Une rafraichissante amertume maîtrisée et ajustée par un note épicée de thym.'
+      }
+    ]
+  },
+  {
+    name: 'Vins',
+    items: [
+      { name: 'Vin Blanc', description: 'Viognier "les îles blanches", 2024' },
+      { name: 'Vin Rouge', description: 'Chapelle Saint-Martin 2024' },
+      { name: 'Verre', price: '3€' },
+      { name: 'Bouteille', price: '12€' }
+    ]
+  },
+  {
+    name: 'Boissons chaudes',
+    items: [
+      { name: 'Infusion du comédien', price: '2€' },
+      { name: 'Chocolat Chaud', price: '2€' },
+      { name: 'Café', price: '2€' },
+      { name: 'Thé', price: '2€' }
+    ]
+  },
+  {
+    name: 'Softs',
+    items: [
+      {
+        name: 'Limonade maison',
+        price: '1,5€',
+        description: 'Citron et menthe'
+      },
+      { name: 'Jus', price: '1,5€' },
+      { name: 'Eau pétillante', price: '1,5€' },
+      { name: 'Coca-Cola', price: '2€' },
+      { name: 'Eau plate', price: '0€', description: 'du robinet' }
+    ]
+  },
+  {
+    name: 'Petite restauration',
+    items: [
+      { name: 'Chips', price: '1,5€' },
+      { name: 'Tarte sucrée', price: '3,5€' },
+      {
+        name: 'Assiette apéro',
+        price: '6€',
+        description: 'Mixte ou végétarienne'
+      },
+      {
+        name: 'Croque-Monsieur',
+        price: '7€',
+        description:
+          'et sa petite salade - voir tableau - vendredi soir et samedi midi'
+      },
+      {
+        name: 'Plat du week-end',
+        price: '12€',
+        description: 'Voir tableau - samedi soir et le dimanche'
+      }
+    ]
+  }
+]
+function App () {
+  return (
+    <>
+      <H1>Au Beau Vignet</H1>
+      <H2>Brasserie éphémère des fêtes de la Saint-Martin</H2>
+
+      <Accordion
+        type='single'
+        collapsible
+        className='w-full'
+        defaultValue='item-1'
+      >
+        {menuItems.map(category => (
+          <AccordionItem key={category.name} value={category.name}>
+            <AccordionTrigger>
+              {category.name}
+              <span className='ml-auto text-right'>{category.size}</span>
+            </AccordionTrigger>
+            <AccordionContent className='flex flex-col gap-4 text-left'>
+              <ul className=''>
+                {category.items.map(item => (
+                  <li key={item.name} className='flex flex-col'>
+                    <div className='flex items-start justify-between'>
+                      <span className='pr-4'>{item.name}</span>
+                      {item.price && (
+                        <span className='text-right whitespace-nowrap'>
+                          {item.price}
+                        </span>
+                      )}
+                    </div>
+
+                    {(item.description || item.abv) && (
+                      <div className='flex items-start justify-between mt-1'>
+                        <p className='text-sm italic mr-4'>
+                          {item.description}
+                        </p>
+                        {item.abv && (
+                          <span className='text-sm italic whitespace-nowrap ml-4'>
+                            {item.abv}
+                          </span>
+                        )}
+                      </div>
+                    )}
+                  </li>
+                ))}
+              </ul>
+            </AccordionContent>
+          </AccordionItem>
+        ))}
+      </Accordion>
+    </>
+  )
+}
+
+export default App
